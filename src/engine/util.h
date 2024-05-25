b8e952eda1ba229334166e6470f040aaa29ecdd3
Nathaniel C. Poteat <ntn.poteat@gmail.com>
2024-05-25 18:32:40 -0400

    Fixed ERROR_EXIT macro in util.h

--- src/engine/util.h
+++ src/engine/util.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 // Macros that we can use instead of having to constantly write error message code.
-#define ERROR_EXIT(...) fprintf(stderr, __VA_ARGS__)
+#define ERROR_EXIT(...) fprintf(stderr, __VA_ARGS__); exit(1)
 #define ERROR_RETURN(R, ...) fprintf(stderr, __VA_ARGS__); return R
 
 #endif
